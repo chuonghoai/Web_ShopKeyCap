7c65716af88160f600e6411685e484d60d9877df
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-05-24 12:22:55 +0700

    refactor client route

--- src/apps/client/routes/client.routes.tsx
+++ src/apps/client/routes/client.routes.tsx
@@ -6,11 +6,23 @@ const ProfilePage = () => {
     return <h1>Thông tin cá nhân (Cần đăng nhập)</h1>;
 };
 
+const HomePage = () => {
+    return <h1>Client Home (Công khai)</h1>;
+};
+
 export const clientRoutes: RouteObject[] = [
+    {
+        path: "/",
+        element: (
+            <AuthGuard requireAuth={false}>
+                <HomePage />
+            </AuthGuard>
+        ),
+    },
     {
         path: "/profile",
         element: (
-            <AuthGuard requireAuth={true} allowedRoles={[ROLE.CLIENT, ROLE.ADMIN]}>
+            <AuthGuard requireAuth={true} allowedRoles={[ROLE.CLIENT]}>
                 <ProfilePage />
             </AuthGuard>
         ),
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -3,15 +3,10 @@ import { useRoutes } from "react-router-dom";
 import { clientRoutes } from "../apps/client/routes/client.routes";
 import { adminRoutes } from "../apps/admin/routes/admin.routes";
 import AuthPage from "../apps/auth/page/layout/authPage";
-import AuthGuard from "../core/auth/auth.guard";
 
 // MOCK DEMO page
 const NotFoundPage = () => {
     return <h1>404 - Không tìm thấy trang</h1>;
-};
-
-const HomePage = () => {
-    return <h1>Client Home (Công khai)</h1>;
 };
 
 export const AppRoutes = () => {
@@ -20,15 +15,6 @@ export const AppRoutes = () => {
         { path: "/register", element: <AuthPage /> },
         { path: "/forgot-password", element: <AuthPage /> },
         { path: "/reset-password", element: <AuthPage /> },
-
-        {
-            path: "/",
-            element: (
-                <AuthGuard requireAuth={false}>
-                    <HomePage />
-                </AuthGuard>
-            ),
-        },
 
         ...clientRoutes,
         ...adminRoutes,
