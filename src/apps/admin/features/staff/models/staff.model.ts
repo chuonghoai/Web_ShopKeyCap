93c85ae1b91e8e24f15ea49338c126d3184bb72a
chuonghoai <144915081+chuonghoai@users.noreply.github.com>
2026-06-19 09:14:44 +0700

    feat: add model admin manage staff

---
+++ src/apps/admin/features/staff/models/staff.model.ts
@@ -0,0 +1,13 @@
+import type { ERole } from "../../../../../core/constants/role.constant";
+
+export interface staffModel {
+    id: number;
+    name: number;
+    email: string;
+    phonenumber: string;
+    dob: Date;
+    gender: 'MALE' | 'FEMALE';
+    salary: number;
+    createAt: number;
+    role: ERole;
+}
